e7a8ccb842af5d7f0ddb188a24306530948ba270
Charlie Sharpsteen <source@sharpsteen.net>
2017-12-04 04:34:25 -0800

    Assert that uploaded facts are stored in PuppetDB

--- test/acceptance/tests/10_test_facts_upload.rb
+++ test/acceptance/tests/10_test_facts_upload.rb
@@ -1,3 +1,18 @@
+require 'json'
+
 step 'Test Puppet Facts upload' do
+  on(hosts, 'mkdir -p /etc/facter/facts.d')
+  create_remote_file(hosts, '/etc/facter/facts.d/test_fact.txt', 'test_fact=foo')
+
   on(hosts, puppet('facts', 'upload'))
+end
+
+step 'Check PuppetDB for uploaded facts' do
+  hosts.each do |host|
+    host_fqdn = on(master, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
+    query = "curl http://localhost:8080/pdb/query/v4/nodes/#{host_fqdn}/facts/test_fact"
+
+    stored_facts = JSON.parse(on(master, query).stdout.chomp)
+    assert_equal('foo', stored_facts.first['value'])
+  end
 end
