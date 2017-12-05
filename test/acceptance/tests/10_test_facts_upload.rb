ae6a44aa840191bee8c4070a46eac54e06bfdf19
Charlie Sharpsteen <source@sharpsteen.net>
2017-12-05 06:36:51 -0800

    Use explict master hostname in acceptance tests
    This patch adds a `--server` flag to the acceptance test for
    `puppet facts upload` and passes the fqdn of the master. Using the default
    `puppet` hostname can be problematic.

--- test/acceptance/tests/10_test_facts_upload.rb
+++ test/acceptance/tests/10_test_facts_upload.rb
@@ -4,6 +4,7 @@ require 'securerandom'
 # Using a random string allows this test to be re-run against staged Beaker VMs
 # without past successes showing up as false positives.
 RANDOM_VALUE = SecureRandom.base64.freeze
+master_fqdn = on(master, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
 
 step 'Test Puppet Facts upload' do
   on(hosts, 'mkdir -p /etc/facter/facts.d')
@@ -11,13 +12,13 @@ step 'Test Puppet Facts upload' do
                      '/etc/facter/facts.d/test_fact.txt',
                      "facts_upload_test_value=#{RANDOM_VALUE}")
 
-  on(hosts, puppet('facts', 'upload'))
+  on(hosts, puppet('facts', 'upload', '--server', master_fqdn))
   sleep(1) # Give PDB some time to process the upload
 end
 
 step 'Check PuppetDB for uploaded facts' do
   hosts.each do |host|
-    host_fqdn = on(master, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
+    host_fqdn = on(host, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
     query = "curl http://localhost:8080/pdb/query/v4/nodes/#{host_fqdn}/facts/facts_upload_test_value"
 
     stored_facts = JSON.parse(on(master, query).stdout.chomp)
