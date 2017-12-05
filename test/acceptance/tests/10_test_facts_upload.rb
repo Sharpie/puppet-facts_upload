b0040c1d92583940463f605209ac436cd3c33747
Charlie Sharpsteen <source@sharpsteen.net>
2017-12-05 00:12:27 -0800

    Use a random fact value in acceptance tests
    This patch uploads the acceptance tests for `puppet facts upload` to use a
    randomly generated value. This allows the acceptance suite to be re-run without
    false positives showing up from past runs.

--- test/acceptance/tests/10_test_facts_upload.rb
+++ test/acceptance/tests/10_test_facts_upload.rb
@@ -1,8 +1,15 @@
 require 'json'
+require 'securerandom'
+
+# Using a random string allows this test to be re-run against staged Beaker VMs
+# without past successes showing up as false positives.
+RANDOM_VALUE = SecureRandom.base64.freeze
 
 step 'Test Puppet Facts upload' do
   on(hosts, 'mkdir -p /etc/facter/facts.d')
-  create_remote_file(hosts, '/etc/facter/facts.d/test_fact.txt', 'test_fact=foo')
+  create_remote_file(hosts,
+                     '/etc/facter/facts.d/test_fact.txt',
+                     "facts_upload_test_value=#{RANDOM_VALUE}")
 
   on(hosts, puppet('facts', 'upload'))
   sleep(1) # Give PDB some time to process the upload
@@ -11,9 +18,9 @@ end
 step 'Check PuppetDB for uploaded facts' do
   hosts.each do |host|
     host_fqdn = on(master, '/opt/puppetlabs/bin/facter fqdn').stdout.chomp
-    query = "curl http://localhost:8080/pdb/query/v4/nodes/#{host_fqdn}/facts/test_fact"
+    query = "curl http://localhost:8080/pdb/query/v4/nodes/#{host_fqdn}/facts/facts_upload_test_value"
 
     stored_facts = JSON.parse(on(master, query).stdout.chomp)
-    assert_equal('foo', stored_facts.first['value'])
+    assert_equal(RANDOM_VALUE, stored_facts.first['value'])
   end
 end
