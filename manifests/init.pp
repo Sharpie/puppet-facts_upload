97a8a8ebb24dda1a6ae9e099a4da15ad36f0c031
Charlie Sharpsteen <source@sharpsteen.net>
2017-12-04 19:15:51 -0800

    Add support for PE
    This patch adds support for the PE build of Puppet Server to the main
    configuration manifest of the module. The main differences are service
    names and patching up the PE configuration structure to match what is
    allowed by the FOSS build.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,57 @@
-# Slots a build of the clojure file server into a PE install. Use this class
-# by adding it to the "PE Masters" group.
+# Slots the facts_upload extension JAR into a PE install. Use this class by
+# adding it to the "PE Masters" group or otherwise applying it to a node
+# running PE or Open Source Puppet Server.
 class facts_upload (
   Enum['present', 'absent'] $ensure = 'present',
 ) {
 
-  # FIXME: Bail if Puppet Server 5.x isn't in use.
-  # TODO: Support Puppet Server 2.6 -- 2.8
+  # FIXME: Bail if Puppet Server 5.x/PE 2017.x isn't in use.
+  # TODO: Support Puppet Server 2.6 -- 2.8/PE 2016.4.x -- 2017.2.x
+
+  if fact('pe_server_version') =~ String {
+    # PE configuration
+
+    if $ensure == 'present' {
+      # PE needs a services.d directory added to the bootstrap path that can be
+      # used to slot in additional services.
+      Pe_ini_setting <| title == "puppetserver initconf bootstrap_config" |> {
+        value => '/etc/puppetlabs/puppetserver/bootstrap.cfg,/etc/puppetlabs/puppetserver/services.d/'
+      }
+
+      # NOTE: An exec is used here instead of a file resource to avoid a
+      # potential duplicate resource issue if this sort of extension module
+      # gets repeated for a different use case.
+      exec {'ensure puppetserver services.d directory existance':
+        command => '/usr/bin/mkdir -m 0755 -p /etc/puppetlabs/puppetserver/services.d',
+        creates => '/etc/puppetlabs/puppetserver/services.d',
+        umask   => '0022',
+        user    => 'root',
+      }
+
+      $_service_file_deps = [Exec['ensure puppetserver services.d directory existance']]
+    } else {
+      $_service_file_deps = []
+    }
+
+    $_puppetserver_service = Exec['pe-puppetserver service full restart']
+  } else {
+    # FOSS configuration
+
+    $_puppetserver_service = Service['puppetserver']
+    $_service_file_deps = []
+  }
 
   file {'/etc/puppetlabs/puppetserver/services.d/facts_upload.cfg':
     ensure => $ensure ? {
       'present' => file,
       'absent'  => absent,
     },
-    owner   => 'puppet',
-    group   => 'puppet',
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     content => "puppetlabs.services.facts-upload.facts-upload-service/facts-upload-service\n",
-    notify  => Service['puppetserver'],
+    require => $_service_file_deps,
+    notify  => $_puppetserver_service,
   }
 
   file {'/opt/puppetlabs/server/data/puppetserver/jars/facts-upload.jar':
@@ -29,7 +64,7 @@ class facts_upload (
     mode   => '0644',
     source => 'puppet:///modules/facts_upload/facts-upload.jar',
     backup => false,
-    notify => Service['puppetserver'],
+    notify => $_puppetserver_service,
   }
 
   puppet_authorization::rule {'node fact upload':
@@ -40,6 +75,6 @@ class facts_upload (
     allow                => '$1',
     sort_order           => 601,
     path                 => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
-    notify               => Service['puppetserver'],
+    notify               => $_puppetserver_service,
   }
 }
